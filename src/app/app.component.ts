a4af3d4a86c591f8dbfd0d21e548b8be4f8929c1
HagerCha <hager.ch2@gmail.com>
2022-04-11 18:11:07 +0100

    firstCommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showCollaborateurBoard = false;
+  showManagerBoard = false;
+  showAssistBoard = false;
+  showPaieBoard = false;
+
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showCollaborateurBoard = this.roles.includes('ROLE_COLLABORATEUR');
+      this.showManagerBoard = this.roles.includes('ROLE_MANAGER');
+      this.showAssistBoard = this.roles.includes('ROLE_ASSIST');
+      this.showPaieBoard = this.roles.includes('ROLE_PAIE');      
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: any = {
+    nom: null,
+    email: null,
+    password: null,
+    passport:null,
+    prenom:null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { nom, email, password ,passport,prenom} = this.form;
+
+    this.authService.register(nom, email, password,passport,prenom).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+}
